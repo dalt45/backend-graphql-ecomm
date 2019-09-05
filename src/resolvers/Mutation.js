1cd056b93265c6512b6c5a473c298504e0ecc938
danielIQ <daniel@mariachi.io>
2019-09-05 16:14:15 -0500

    new features

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,22 +1,79 @@
-const Mutations = {
-	async createItem(parent, args, ctx, info){
-		//TODO check if they're logged in
-		const item = await ctx.db.mutation.createItem({
-			data:{
-				...args
-			}
-		}, info);
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-		console.log(item)
-		return item
-	}
-	/*createDog(parent, args, ctx, info){
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO check if they're logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+
+    console.log(item);
+    return item;
+  },
+  /* createDog(parent, args, ctx, info){
 		//create dog!
 		global.dogs = global.dogs || [];
 		const newDog = {name: args.name}
 		global.dogs.push(newDog)
 		return newDog
-	}*/
+	} */
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the updates
+    const updates = { ...args };
+    // remove id from the updates
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{id title}`);
+    // 2. check if they own the item or have permissions
+    // TODO
+    // 3. Delete it
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
+
+  async signup(parent, args, ctx, info) {
+    // change email to lowercase
+    args.email = args.email.toLowerCase();
+    // hash the password
+    const password = await bcrypt.hash(args.password, 10);
+    // create the user in the database
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] },
+        },
+      },
+      info
+    );
+    // create the JWT for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // We set the jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1year cookie
+    });
+    return user;
+  },
 };
 
 module.exports = Mutations;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,14 +1,17 @@
-const { forwardTo } = require('prisma-binding')
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
-	items: forwardTo('db')
-/*	dogs (parent, args, ctx, info) {
+  items: forwardTo('db'),
+  item: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
+  /*	dogs (parent, args, ctx, info) {
 		global.dogs = global.dogs || [];
 		return global.dogs
-	}*/
-/*async items(parent, args, ctx, info){
+	} */
+  /* async items(parent, args, ctx, info){
 	const items = await ctx.db.query.items()
 	return items
-}*/
+} */
 };
 
 module.exports = Query;
